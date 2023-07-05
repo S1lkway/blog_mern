47f9379667eccef9f06af6d195143163a3793ae9
Artem <silkwayway@gmail.com>
2023-07-05 14:19:49 -0400

    fix errorMiddleware and add connection to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,27 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
-const colors = require('colors')
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
+
+connectDB()
 
 const app = express()
 
-//Middleware is used to parse incoming requests with JSON payloads
+/* Used to parse incoming requests with JSON payloads */
 app.use(express.json())
-/* Middleware is used to parse incoming requests with URL-encoded payloads.
-The 'extended' option determines how the URL-encoded data is parsed. When set to false, the data is parsed using the 'querystring' */
+/* Used to parse incoming requests with URL-encoded payloads 
+The extended option determines how the URL-encoded data is parsed. When set to false, the data is parsed using the querystring*/
 app.use(express.urlencoded({ extended: false }))
 
 
+//ROUTES
 
+
+
+/* Provides a basic error handling mechanism in Express that sends a JSON response with an error message and, optionally, the stack trace */
 app.use(errorHandler)
+
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
