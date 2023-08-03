b449139bde10b0ba1f529d009e16188f6e175d3b
Artem <silkwayway@gmail.com>
2023-08-03 17:31:28 -0400

    final commit

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,21 +1,23 @@
+import { HiGlobeAlt } from "react-icons/hi2";
+
 function Main() {
   return (
-    <div>
-      <h2>Welcome to our exciting news portal!</h2>
-      <div className="welcomeText">
-        <p>Here, you have a unique opportunity not only to stay informed about the latest events but also to become an active participant in our community of news enthusiasts.</p>
+    <div className="welcomeText">
+      <h1><HiGlobeAlt /> Welcome to our exciting news portal!</h1>
 
-        <p>Want to share your own news or interesting events from your region? Simply register on the website and create your own news articles. Share your views and information with other readers!</p>
+      <p>Here, you have a unique opportunity not only to stay informed about the latest events but also to become an active participant in our community of news enthusiasts.</p>
 
-        <p>Moreover, we provide the option to edit news articles. If you find inaccuracies or want to add more content, feel free to do so! Use the editing feature to make your news even better.</p>
+      <p>Want to share your own news or interesting events from your region? Simply register on the website and create your own news articles. Share your views and information with other readers!</p>
 
-        <p>At our portal, we value communication and free expression of opinions. Comments are a great way to discuss news with other readers. You can express your thoughts, support the authors, or ask intriguing questions.</p>
+      <p>Moreover, we provide the option to edit news articles. If you find inaccuracies or want to add more content, feel free to do so! Use the editing feature to make your news even better.</p>
 
-        <p>We believe that everyone has the right to their opinion, and all opinions are valuable here. Be respectful to one another and appreciate the diversity of viewpoints.</p>
+      <p>At our portal, we value communication and free expression of opinions. Comments are a great way to discuss news with other readers. You can express your thoughts, support the authors, or ask intriguing questions.</p>
 
-        <p>Join our friendly community of news enthusiasts and stay at the forefront of events with us!</p>
-      </div>
+      <p>We believe that everyone has the right to their opinion, and all opinions are valuable here. Be respectful to one another and appreciate the diversity of viewpoints.</p>
+
+      <p>Join our friendly community of news enthusiasts and stay at the forefront of events with us!</p>
     </div>
+
   )
 }
 
