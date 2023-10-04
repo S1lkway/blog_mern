2f5abb5f876532069b12cbaf0e09752e7669130c
Artem <silkwayway@gmail.com>
2023-10-04 12:55:08 -0400

    pre deploy fix

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,6 @@ const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
 
-
-connectDB()
 const app = express()
 
 
@@ -34,7 +32,23 @@ app.use('/api/articles', require('./routes/articleRoutes'))
 // routes for reading all articles, make comments and like them
 app.use('/api/news', require('./routes/newsRoutes'))
 
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  )
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'))
+}
+
 /* Provides a basic error handling mechanism in Express that sends a JSON response with an error message and, optionally, the stack trace */
 app.use(errorHandler)
 
-app.listen(port, () => console.log(`Server started on port ${port}`.yellow.underline))
+
+connectDB().then(() => {
+  app.listen(port, () => console.log(`Server started on port ${port}`.yellow.underline))
+})
--- frontend/src/utils/PrivateRoutes.jsx
+++ frontend/src/utils/PrivateRoutes.jsx
@@ -1,14 +1,32 @@
 import { Outlet, Navigate, useLocation } from "react-router-dom"
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from 'react-redux'
+import jwt_decode from "jwt-decode";
+// - Redux
+import { logout, reset } from '../features/auth/authSlice'
+import { resetArticles } from '../features/articles/articleSlice'
+import { resetNews } from '../features/news/newsSlice'
+
 
 function PrivateRoutes() {
+  const dispatch = useDispatch()
   const location = useLocation();
   const user = useSelector((state) => state.auth.user);
+  const prevUrl = location.pathname;
 
   if (user != null) {
+    const token = user.token
+    const decodedToken = jwt_decode(token);
+    const currentTime = Date.now() / 1000;
+    if (decodedToken.exp < currentTime) {
+      dispatch(logout())
+      dispatch(reset())
+      dispatch(resetArticles())
+      dispatch(resetNews())
+
+      return <Navigate to="/login" state={{ 'prevUrl': prevUrl }} />
+    }
     return <Outlet />
   } else {
-    const prevUrl = location.pathname;
     return <Navigate to="/login" state={{ 'prevUrl': prevUrl }} />
   }
 
